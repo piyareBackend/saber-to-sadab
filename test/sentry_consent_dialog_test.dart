2452affef984b7d4b9cd0d1f7aeadc0a97f4a30b
piyareBackend <sadab.notes.backup@gmail.com>
2026-09-09 12:27:27 +0530

    fix: migrate sentry consent golden test to Sadab package

--- test/sentry_consent_dialog_test.dart
+++ test/sentry_consent_dialog_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_screenshot/golden_screenshot.dart';
-import 'package:saber/data/flavor_config.dart';
-import 'package:saber/data/prefs.dart';
-import 'package:saber/data/sentry/sentry_init.dart';
-import 'package:saber/pages/home/home.dart';
+import 'package:sadab/data/flavor_config.dart';
+import 'package:sadab/data/prefs.dart';
+import 'package:sadab/data/sentry/sentry_init.dart';
+import 'package:sadab/pages/home/home.dart';
 
 void main() {
   group('SentryConsentDialog', () {
