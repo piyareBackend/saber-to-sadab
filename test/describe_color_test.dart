8057e59d6f2c32d02ade34837b43438f5005d398
piyareBackend <sadab.notes.backup@gmail.com>
2026-09-09 12:26:34 +0530

    fix: migrate color description test to Sadab package

--- test/describe_color_test.dart
+++ test/describe_color_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:saber/components/toolbar/color_bar.dart';
+import 'package:sadab/components/toolbar/color_bar.dart';
 
 void main() {
   group('describeColor', () {
