dcd3f960dc94aa3a83803c9061c0fa48b01b2101
piyareBackend <sadab.notes.backup@gmail.com>
2026-09-09 12:26:44 +0530

    fix: migrate browse golden test to Sadab package

--- test/browse_page_test.dart
+++ test/browse_page_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_screenshot/golden_screenshot.dart';
-import 'package:saber/components/theming/saber_theme.dart';
-import 'package:saber/data/file_manager/file_manager.dart';
-import 'package:saber/data/flavor_config.dart';
-import 'package:saber/pages/home/browse.dart';
+import 'package:sadab/components/theming/saber_theme.dart';
+import 'package:sadab/data/file_manager/file_manager.dart';
+import 'package:sadab/data/flavor_config.dart';
+import 'package:sadab/pages/home/browse.dart';
 
 import 'utils/test_mock_channel_handlers.dart';
 
