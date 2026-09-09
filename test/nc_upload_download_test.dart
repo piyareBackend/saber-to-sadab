b51037564832aae87ea062d5d1eeda516ade291d
piyareBackend <sadab.notes.backup@gmail.com>
2026-09-09 11:58:30 +0530

    fix(sadab): remove committed Nextcloud test credentials

--- test/nc_upload_download_test.dart
+++ test/nc_upload_download_test.dart
@@ -10,56 +10,72 @@ import 'package:sadab/data/prefs.dart';
 import 'utils/test_mock_channel_handlers.dart';
 import 'utils/test_random.dart';
 
-void main() async {
-  test('Upload and download file', () async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    HttpOverrides.global = null; // enable http requests in test
-    setupMockPathProvider();
-    setupMockFlutterSecureStorage();
+void main() {
+  final username = Platform.environment['SADAB_NC_USERNAME'];
+  final password = Platform.environment['SADAB_NC_PASSWORD'];
+  final serverUrl = Platform.environment['SADAB_NC_URL'];
+  final hasIntegrationConfig =
+      username != null && username.isNotEmpty &&
+      password != null && password.isNotEmpty &&
+      serverUrl != null && serverUrl.isNotEmpty;
 
-    FileManager.documentsDirectory =
-        '$tmpDir/nc_upload_download_test/'
-        '${FileManager.appRootDirectoryPrefix}';
-    FlavorConfig.setup();
-    await FileManager.init();
+  test(
+    'Upload and download file',
+    () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      HttpOverrides.global = null;
+      setupMockPathProvider();
+      setupMockFlutterSecureStorage();
 
-    stows.username.value = 'test.issue.118';
-    stows.ncPassword.value = 'riNLA-2fXWY-Kay3x-jEMX5-bZr6m';
-    stows.encPassword.value = 'test.issue.118';
+      FileManager.documentsDirectory =
+          '$tmpDir/nc_upload_download_test/'
+          '${FileManager.appRootDirectoryPrefix}';
+      FlavorConfig.setup();
+      await FileManager.init();
 
-    final client = SaberSyncInterface.client!;
-    await client.loadEncryptionKey();
+      stows.url.value = serverUrl!;
+      stows.username.value = username!;
+      stows.ncPassword.value = password!;
+      stows.encPassword.value = username;
 
-    final localFile = FileManager.getFile('/helloworld${randomString(10)}.txt');
-    final syncFile = await syncer.interface.getSyncFileFromLocalFile(localFile);
-    printOnFailure(syncFile.toString());
-    expect(syncFile.remotePath, matches(RegExp(r'^Saber/[a-zA-Z0-9]+\.sbe$')));
+      final client = SaberSyncInterface.client!;
+      await client.loadEncryptionKey();
 
-    const content = 'Hello, world!';
-    await localFile.create(recursive: true);
-    await localFile.writeAsString(content);
+      final localFile = FileManager.getFile('/helloworld${randomString(10)}.txt');
+      final syncFile = await syncer.interface.getSyncFileFromLocalFile(localFile);
+      printOnFailure(syncFile.toString());
+      expect(syncFile.remotePath, matches(RegExp(r'^Saber/[a-zA-Z0-9]+\.sbe$')));
 
-    final upBytes = await syncer.interface.readLocalFile(syncFile);
-    expect(upBytes, isNotEmpty);
-    await syncer.interface.uploadRemoteFile(syncFile, upBytes);
+      const content = 'Hello, world!';
+      await localFile.create(recursive: true);
+      await localFile.writeAsString(content);
 
-    final remoteFile = await syncer.interface.getWebDavFile(
-      syncFile.remotePath,
-    );
-    if (remoteFile == null) fail('Remote file not found after upload');
-    final syncFile2 = await syncer.interface.getSyncFileFromRemoteFile(
-      remoteFile,
-    );
-    expect(syncFile2, equals(syncFile));
+      final upBytes = await syncer.interface.readLocalFile(syncFile);
+      expect(upBytes, isNotEmpty);
+      await syncer.interface.uploadRemoteFile(syncFile, upBytes);
 
-    final downBytes = await syncer.interface.downloadRemoteFile(syncFile);
-    expect(downBytes, isNotEmpty);
-    expect(downBytes, equals(upBytes));
-    await syncer.interface.writeLocalFile(
-      syncFile,
-      downBytes,
-      awaitWrite: true,
-    );
-    expect(await localFile.readAsString(), equals(content));
-  }, retry: 2);
+      final remoteFile = await syncer.interface.getWebDavFile(
+        syncFile.remotePath,
+      );
+      if (remoteFile == null) fail('Remote file not found after upload');
+      final syncFile2 = await syncer.interface.getSyncFileFromRemoteFile(
+        remoteFile,
+      );
+      expect(syncFile2, equals(syncFile));
+
+      final downBytes = await syncer.interface.downloadRemoteFile(syncFile);
+      expect(downBytes, isNotEmpty);
+      expect(downBytes, equals(upBytes));
+      await syncer.interface.writeLocalFile(
+        syncFile,
+        downBytes,
+        awaitWrite: true,
+      );
+      expect(await localFile.readAsString(), equals(content));
+    },
+    skip: hasIntegrationConfig
+        ? false
+        : 'Nextcloud integration credentials are not configured in CI',
+    retry: 2,
+  );
 }
